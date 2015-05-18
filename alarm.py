f111de15e9cd4460aed7980bedca5d86b881d156
Thomas Lim <txl11@ic.ac.uk>
2015-05-18 11:56:23 +0100

    Was accidentally getting the series PID rather than the episode PID

--- alarm.py
+++ alarm.py
@@ -1,4 +1,5 @@
 from datetime import date
+from subprocess import call
 
 import requests
 from bs4 import BeautifulSoup
@@ -9,6 +10,8 @@ from bs4 import BeautifulSoup
 
 schedule_url = date.today().strftime("http://www.bbc.co.uk/radio4/programmes/schedules/%Y/%m/%d")
 
+print schedule_url
+
 r = requests.get(schedule_url)
 
 soup = BeautifulSoup(r.text)
@@ -18,9 +21,16 @@ morning_sec = soup.find(id='morning')
 programmes = morning_sec.findAll("div", {"class": "programme__body"})
 
 for p in programmes:
-    if p.find("span", {"class": "programme__title"}).text == " Today ":
-        pid = p.find("span", {"class": "programme__title"}).span['resource']
 
+    # print p.find("span", {"class": "programme__title"}).text
+    # print "Today" in p.find("span", {"class": "programme__title"}).text
+
+    if "Today" in p.find("span", {"class": "programme__title"}).text:
+        # pid = p.find("span", {"class": "programme__title"}).span['resource'].split('/')[-1]
+        pid = p.a['href'].split("/")[-1]
+        print "Found Today. PID = %s" % pid
 
 
 # Download the programme
+
+call(["get_iplayer", "--type=radio", "--get", "--aactomp3", "--pid=%s" % pid])
